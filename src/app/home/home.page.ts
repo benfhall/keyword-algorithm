95b97787b2ccec0c85c97779a0ce8a36c13b4513
Ben Hall <swidexx@gmail.com>
2019-08-21 21:59:17 -0500

    init ionic application, imported code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,28 @@ export class HomePage {
 
   constructor() {}
 
+  searchKeyword() {
+
+    var results = []
+    results.length = 0;
+
+    let comments = document.getElementById("comments-box").value
+    const commentsArray = comments.split("\n");
+    var keyword = document.getElementById("keyword-box").value
+
+    console.log(commentsArray)
+    console.log(keyword)
+
+    for (var i = 0; i <= (commentsArray.length - 1); i++) {
+      console.log(commentsArray[i])
+      if (commentsArray[i].toLowerCase().includes(keyword.toLowerCase())) {
+        results += "<ion-item>" + commentsArray[i] + "</ion-item>"
+      }
+    }
+
+    document.getElementById("result-box").innerHTML = results
+    console.log(results)
+
+  }
+
 }
